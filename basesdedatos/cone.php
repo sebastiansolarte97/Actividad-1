f2fd12e07c0df229eda1578aee0cc764031fc6fe
sebastiansolarte97 <36212036+sebastiansolarte97@users.noreply.github.com>
2018-02-20 13:59:51 -0500

    Delete cone.php

--- basesdedatos/cone.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conexion= mysqli_connect("localhost", "root", "", "diaro");
-//if (!$conexion) {
-//	echo "Error al conectar la base de datos";
-//} 
-//else {
-//	echo "conectado a una base de datos";
-//} 
